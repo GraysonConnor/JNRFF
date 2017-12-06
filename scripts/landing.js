811530e9bad0850c110f61a10577dc902719cdf5
Grayson Connor <grayson.bolon@yahoo.com>
2017-12-05 21:35:50 -0600

    added to js file

---
+++ scripts/landing.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+$('#icon1').on('click', function(){
+  $('.ion-social-youtube-outline').slideUp(1300);
+  $('.icon-point1').fadeOut(1300);
+
+});
+
+$('#icon2').on('click', function(){
+  $('.ion-ios-mic-outline').slideUp(1300);
+  $('.icon-point2').fadeOut(1300);
+
+});
+
+$('#icon3').on('click', function(){
+  $('.ion-ios-person-outline').slideUp(1300);
+  $('.icon-point3').fadeOut(1300);
+
+});
+
+$('#icon4').on('click', function(){
+  $('.ion-ios-email-outline').slideUp(1300);
+  $('.icon-point4').fadeOut(1300);
+
+});
+});
